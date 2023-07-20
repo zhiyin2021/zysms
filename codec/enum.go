a3bc4ef39096153aba6a84797b2ad0b496c06afb
IT757 <2870418@qq.com>
2023-07-20 16:13:07 +0800

    version.string

--- codec/enum.go
+++ codec/enum.go
@@ -25,10 +25,28 @@ var protoMap = map[SmsProto]Version{
 	SMPP33: 0x33,
 	SMPP34: 0x34,
 }
+var protoStr = map[SmsProto]string{
+	CMPP20: "cmpp20",
+	CMPP21: "cmpp21",
+	CMPP30: "cmpp30",
+	SMGP13: "smgp13",
+	SMGP20: "smgp20",
+	SMGP30: "smgp30",
+	SGIP:   "sgip",
+	SMPP33: "smpp33",
+	SMPP34: "smpp34",
+}
 
 func (s SmsProto) Version() Version {
 	if v, ok := protoMap[s]; ok {
 		return v
 	}
 	return 0
+}
+
+func (s SmsProto) String() string {
+	if v, ok := protoStr[s]; ok {
+		return v
+	}
+	return "known"
 }
