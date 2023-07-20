50458931fe0c412860adcfb00d943506713910d3
IT757 <2870418@qq.com>
2023-07-20 11:39:24 +0800

    smsproto 转 version

--- codec/enum.go
+++ codec/enum.go
@@ -13,3 +13,22 @@ const (
 	SMPP33
 	SMPP34
 )
+
+var protoMap = map[SmsProto]Version{
+	CMPP20: 0x20,
+	CMPP21: 0x21,
+	CMPP30: 0x30,
+	SMGP13: 0x13,
+	SMGP20: 0x20,
+	SMGP30: 0x30,
+	SGIP:   0x12,
+	SMPP33: 0x33,
+	SMPP34: 0x34,
+}
+
+func (s SmsProto) Version() Version {
+	if v, ok := protoMap[s]; ok {
+		return v
+	}
+	return 0
+}
